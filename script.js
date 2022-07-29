a1d7ec11f97a533088a52b11f2ae84cec1d44e16
shrutimannikeri <33412806+shrutimannikeri@users.noreply.github.com>
2022-07-29 16:21:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    let result=document.querySelector("#result")
+    let result=document.querySelector("#resultt")
+    //input filed just for test
+    let resultt=document.querySelector("#result")
     let history = document.querySelector('#history');
 
     let clear=document.querySelector("#clear")
-    let deletesinglenum=document.querySelector("#delete_single_value")
-    let operator_btn = document.querySelectorAll('#operator_btn');
-    let equalto=document.querySelector("#equalto")
+    let deletesinglenum=document.querySelector(".delete_single_value")
+    let operator_btn = document.querySelectorAll('.operator_btn');
+    let equalto=document.querySelector("#equal")
     let initialvalue = "";
     //it iterate all  operator_btn one by one it will 
     operator_btn.forEach((operator_btn,ind)=>{
         operator_btn.addEventListener("click",function(){
-            let text=this.innerText
+            let text=this.innerHTML
             initialvalue =initialvalue+text;
             result.innerHTML = initialvalue;
             //console.log( result.innerHTML = initialvalue)
@@ -22,6 +24,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         history.innerHTML=result.innerHTML
         result.innerHTML=eval(result.innerHTML);
         initialvalue=eval(result.innerHTML)
+        resultt.value=eval(result.innerHTML)
         }else{
             alert('please enter any Number');
             }
